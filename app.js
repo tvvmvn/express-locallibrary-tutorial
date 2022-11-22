c908e869f4ce27d3753c90c98d05ee92c390b913
tvvmvn <tvvmvn@gmail.com>
2022-11-22 15:44:55 +0900

    .

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" 
 var compression = require('compression');
 var helmet = require('helmet');
 
-// require('dotenv').config()
-// console.log(process.env) // remove this after you've confirmed it is working
-
 var app = express();
 
 // Set up mongoose connection
