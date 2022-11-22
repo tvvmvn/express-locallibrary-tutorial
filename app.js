90f78d72b15d691e5e386723f5653043b6619ea7
tvvmvn <tvvmvn@gmail.com>
2022-11-22 15:21:30 +0900

    .

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var app = express();
 // Set up mongoose connection
 var mongoose = require('mongoose');
 // var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var dev_db_url = 'mongodb+srv://tvvmvn:2dXThsbGeJyrt93p@cluster0.eho7r.mongodb.net/local_library?retryWrites=true'
+console.log(process.env.MONGODB_URI);
+var mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
